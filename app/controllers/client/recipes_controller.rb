b2615a560294d03514c1c3ce5f79426b58628465
farfalle211 <farfalle211@gmail.com>
2019-02-07 13:14:18 -0600

    cookbook client app

---
+++ app/controllers/client/recipes_controller.rb
@@ -0,0 +1,42 @@
+class Client::RecipesController < ApplicationController
+
+  def index
+    response = HTTP.get("http://localhost:3000/api/recipes")
+
+    @recipes = response.parse
+    render 'index.html.erb'
+  end
+    
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    client_params = { 
+                      title: params[:title], 
+                      chef: params[:chef],
+                      prep_time: params[:prep_time],
+                      ingredients: params[:ingredients],
+                      directions: params[:directions],
+                      image_url: params[:image_url]
+                      }
+
+    response = HTTP.post(
+                          "http://localhost:3000/api/recipes", 
+                          form: client_params
+                          )
+
+    recipe = response.parse
+    redirect_to "/client/recipes/#{ recipe["id"] }"
+
+  end
+
+  def show
+    # @recipe = Recipe.find(params[:id])
+    response = HTTP.get("http://localhost:3000/api/recipes/#{params[:id]}")
+    @recipe = response.parse
+    render 'show.html.erb'
+  end
+
+end
